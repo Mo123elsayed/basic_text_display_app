f2867d9faefb8c57689690bb977e3c95e7eee79d
Muhammed Elsayed <133232940+Mo123elsayed@users.noreply.github.com>
2025-10-25 21:25:27 +0300

    `Removed ScreenUtilInit and its related code from DisplayText widget`

--- lib/display_text.dart
+++ lib/display_text.dart
@@ -1,22 +1,16 @@
 import 'package:basic_text_display_app/core/routing/app_router.dart';
 import 'package:basic_text_display_app/core/routing/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DisplayText extends StatelessWidget {
-  final AppRouter appRouter ;
+  final AppRouter appRouter;
   const DisplayText({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (context, child) {
-        return MaterialApp(
-          initialRoute: Routes.home,
-          onGenerateRoute: appRouter.generateRoute,
-        );
-      },
+    return MaterialApp(
+      initialRoute: Routes.home,
+      onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
